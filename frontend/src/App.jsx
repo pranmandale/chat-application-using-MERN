7b8cab3c62a089cfc622b318c01a5710b3c6e781
pranmandale <pranavmandale2025@gmail.com>
2024-08-24 20:16:59 +0530

    signup and login frontend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Left from "./Home/left/Left"
 import Right from "./Home/right/Right"
+import Signup from './components/Signup'
+import Login from './components/Login'
 
 function App() {
   return (
-    <div className='flex h-screen'>
-     <Left/>
-     <Right/>     
-    </div>
+    // <div className='flex h-screen'>
+    //  <Left/>
+    //  <Right/>     
+    // </div>
+    // <Signup/>
+    <Login/>
   )
 }
 
